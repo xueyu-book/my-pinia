fca36a01169cf82fcb839777ba71f3079d2ebe40
liaogengyu <liaogengyu@xinli001.com>
2023-01-30 11:48:13 +0800

    change ts to js

---
+++ src/views/CalendarCanvas/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w1000 top">
+    <div ref="divRef">
+      fafa
+    </div>
+    <button @click="clickImage">生成图片</button>
+    <PrintImage :divRef="divRef" ref="PrintRef"/>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import PrintImage from "@/components/image.vue";
+const divRef = ref();
+
+const PrintRef = ref();
+const clickImage = () => {
+  PrintRef.value.clickImage()
+};
+
+</script>
